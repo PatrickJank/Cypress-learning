349e6d8617e487afed6d88f1e535fe1b2c064a36
forma <forma@PAPRYK>
2024-02-06 16:45:48 +0100

    Add project files.

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,12 @@
+//cypress - Spec
+describe('My first test suite! Aka test run', function()
+{
+
+    it('My first testcase', function () {
+
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+
+        // test step
+    })
+
+})
